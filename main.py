a69821faa2c273ab199b2da6fded63848884b417
zkwolf <chenhao5866@gmail.com>
2015-10-23 18:02:52 +0800

    最终版本
    测试没有出现什么问题，添加requirements.txt，添加了一些测试得到的数据

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
 import requests
-from bs4 import BeautifulSoup
 import time
 import datetime
 import os
-
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class GetPage:
 
@@ -13,45 +14,54 @@ class GetPage:
 		self.page = args.u
 		self.directory = args.o
 		self.requests = requests
+		self.requests.keep_alive = False
 		self.headers = {
 						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36'
 						}
 
 	def get_data(self):
-		curr_dir = os.getcwd().replace('\\', '/')
-		os.makedirs(curr_dir + self.directory, exist_ok=True)
 		now = datetime.datetime.now()
 		format_now = now.strftime('%Y%m%d%H%M')
-		print(format_now)
-		self.requests.keep_alive = False
-		postfix = '/?_trans_=000019_jrjs_sy'
-		data = self.requests.get(self.page + postfix, headers=self.headers)
-		# if data.status_code == 200:
-		# 	location = data.headers['Location']
-		# 	data = self.requests.get(location, headers=self.headers)
-		soup = BeautifulSoup(data.text, 'html.parser')
+		print('get page time ' + str(format_now))
+				
+		dcap = dict(DesiredCapabilities.PHANTOMJS)
+		dcap["phantomjs.page.settings.userAgent"] = (self.headers['User-Agent'])
+		driver = webdriver.PhantomJS(desired_capabilities=dcap)
+		driver.get('http://m.sohu.com')
+		data = driver.page_source
+		driver.quit()
+		
+		soup = BeautifulSoup(data, 'html.parser')
+		
+		curr_dir = os.getcwd().replace('\\', '/')
+		os.makedirs(curr_dir + self.directory, exist_ok=True)		
 		os.chdir(curr_dir + self.directory)
 		os.makedirs(format_now, exist_ok=True)
 		os.chdir(format_now)
 		os.makedirs('css', exist_ok=True)
 		os.makedirs('js', exist_ok=True)
 		os.makedirs('image', exist_ok=True)
+		#create directory
+		
 		os.chdir('css')
-		css = soup.find_all('link', attrs = {'type'= 'text/css'})
+		css = soup.find_all('link', attrs = {'type': 'text/css'})
 		for i in css:
 			css_data = self.requests.get(i['href'], headers=self.headers)
 			_, name = os.path.split(i['href'])
 			with open(name, 'wb') as f:
 				f.write(css_data.text.encode('utf-8'))
 			i['href'] = 'css/' + name
+			
 		os.chdir('../js')
-		js = soup.find_all('script', attrs = {'type'= 'text/javascript'})
-		for i in js[1:]:
-			js_data = self.requests.get(i['src'], headers=self.headers)
-			_, name = os.path.split(i['src'])
-			with open(name, 'wb') as f:
-				f.write(js_data.text.encode('utf-8'))
-			i['src'] = 'js/' + name
+		js = soup.find_all('script', attrs = {'type': 'text/javascript'})
+		for i in js:
+			if i.get('src'):
+				js_data = self.requests.get(i['src'], headers=self.headers)
+				_, name = os.path.split(i['src'])
+				with open(name, 'wb') as f:
+					f.write(js_data.text.encode('utf-8'))
+				i['src'] = 'js/' + name
+			
 		os.chdir('../image')
 		image = soup.find_all('img')
 		for i in image:
@@ -61,7 +71,7 @@ class GetPage:
 				f.write(image_data.content)
 			i['src'] = 'image/' + name
 			
-		os.chdir(..)
+		os.chdir('..')
 		with open('index.html', 'wb') as f:
 			f.write(soup.encode('utf-8'))
 		os.chdir(curr_dir)
