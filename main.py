1a5647b033a1cd84b9f5b549d16fb5bce7af16d7
zkwolf <chenhao5866@gmail.com>
2015-10-23 11:59:26 +0800

    下载后的处理已完成，获取网页还有问题

--- main.py
+++ main.py
@@ -22,46 +22,55 @@ class GetPage:
 		os.makedirs(curr_dir + self.directory, exist_ok=True)
 		now = datetime.datetime.now()
 		format_now = now.strftime('%Y%m%d%H%M')
-		request = self.requests
-		request.keep_alive = False
-		temp = request.get(self.page, headers=self.headers)
-		# the temp to get redirect url
-		data = request.get(temp.url, headers=self.headers)
+		print(format_now)
+		self.requests.keep_alive = False
+		postfix = '/?_trans_=000019_jrjs_sy'
+		data = self.requests.get(self.page + postfix, headers=self.headers)
+		# if data.status_code == 200:
+		# 	location = data.headers['Location']
+		# 	data = self.requests.get(location, headers=self.headers)
 		soup = BeautifulSoup(data.text, 'html.parser')
 		os.chdir(curr_dir + self.directory)
 		os.makedirs(format_now, exist_ok=True)
 		os.chdir(format_now)
-		with open('index.html', 'wb') as f:
-			f.write(data.text.encode('utf-8'))
-		os.makedirs('css')
-		os.makedirs('js')
-		os.makedirs('image')
+		os.makedirs('css', exist_ok=True)
+		os.makedirs('js', exist_ok=True)
+		os.makedirs('image', exist_ok=True)
 		os.chdir('css')
-		css = {x.attrs.get('href') for x in soup.select('link[type="text/css"]')}
+		css = soup.find_all('link', attrs = {'type'= 'text/css'})
 		for i in css:
-			if i:
-				css_data = request.get(i, headers=self.headers)
-				_, name = os.path.split(i)
-				with open(name, 'wb') as f:
-					f.write(css_data.text.encode('utf-8'))
+			css_data = self.requests.get(i['href'], headers=self.headers)
+			_, name = os.path.split(i['href'])
+			with open(name, 'wb') as f:
+				f.write(css_data.text.encode('utf-8'))
+			i['href'] = 'css/' + name
 		os.chdir('../js')
-		js = {x.attrs.get('src') for x in soup.select('script[type="text/javascript]')}
-		for i in js:
-			if i:
-				js_data = request.get(i, headers=self.headers)
-				_, name = os.path.split(i)
-				with open(name, 'wb') as f:
-					f.write(js_data.text.encode('utf-8'))
+		js = soup.find_all('script', attrs = {'type'= 'text/javascript'})
+		for i in js[1:]:
+			js_data = self.requests.get(i['src'], headers=self.headers)
+			_, name = os.path.split(i['src'])
+			with open(name, 'wb') as f:
+				f.write(js_data.text.encode('utf-8'))
+			i['src'] = 'js/' + name
 		os.chdir('../image')
-		image = {x.attrs.get('src') for x in soup.select('img')}
+		image = soup.find_all('img')
 		for i in image:
-			if i:
-				image_data = request.get(i, headers=self.headers)
-				_, name = os.path.split(i)
-				with open(name, 'wb') as f:
-					f.write(image_data.text.encode('utf-8'))
+			image_data = self.requests.get(i['src'], headers=self.headers)
+			_, name = os.path.split(i['src'])
+			with open(name, 'wb') as f:
+				f.write(image_data.content)
+			i['src'] = 'image/' + name
+			
+		os.chdir(..)
+		with open('index.html', 'wb') as f:
+			f.write(soup.encode('utf-8'))
+		os.chdir(curr_dir)
 
-
+	def run(self):
+		while True:
+			self.get_data()
+			time.sleep(self.interval)
+			
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 
@@ -69,9 +78,7 @@ if __name__ == "__main__":
 	parser.add_argument('-u', help="Set page address")
 	parser.add_argument('-o', help="Set save file")
 
-	args = parser.parse_args(['-d', '60', '-u', 'http://m.sohu.com', '-o', '/tmp/backup'])
+	args = parser.parse_args()
+	#['-d', '60', '-u', 'http://m.sohu.com', '-o', '/tmp/backup']
 	getpage = GetPage()
-	getpage.get_data()
-	
-
-
+	getpage.run()
